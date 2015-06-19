35beb93c6a7d73e61ae31c190411e291672dc6ab
dzito <dan.zito@adroll.com>
2015-06-19 10:12:49 -0700

    new gulp tasks: 'watch' for changes to scripts and 'transform' them

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
+  rename = require('gulp-rename'),
   react = require('gulp-react'),
+  path = require('path'),
   browserSync = require('browser-sync'),
   reload = browserSync.reload;
   
-
+var paths = {
+  scripts: ['lesson_*/src/*.js']
+};
+ 
 gulp.task('server', function() {
   browserSync({
     server: {
@@ -13,4 +18,17 @@ gulp.task('server', function() {
   });
 });
 
-gulp.task('default', ['server']);
+gulp.task('transform', function(){
+  return gulp.src(paths.scripts)
+    .pipe(react())
+    .pipe(rename(function(file) {
+        file.dirname = file.dirname.replace('/src', '/build');
+    }))
+    .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['transform', reload]);
+});
+
+gulp.task('default', ['server', 'watch']);
