e6e7b613f80472433f2c080f8bc1cc0feda39e27
Grant Toeppen <grant@adroll.com>
2015-06-29 10:37:25 -0700

    Update Lesson 1 with correct solution

--- lesson_1/build/solution.js
+++ lesson_1/build/solution.js
@@ -1,33 +1,35 @@
 // Lesson 1 Solution - solution.js
 
-var HelloWorld = React.createClass({displayName: "HelloWorld",
-
-  getDefaultProps: function() {
-    return {
-      name: 'User'
+var TitleBar = React.createClass({displayName: "TitleBar",
+    getDefaultProps: function() {
+        return {
+            title: 'A Title',
+            subtitle: 'Just a Subtitle'
+        }
+    },
+    getInitialState: function() {
+        return {
+            likes: 0
+        }
+    },
+    handleClick: function() {
+        this.setState({
+            likes: this.state.likes + 1
+        });
+    },
+    render: function () {
+        return (
+            React.createElement("div", null, 
+                React.createElement("h1", null, this.props.title), 
+                React.createElement("h2", null, this.props.subtitle), 
+                React.createElement("span", null, "Likes: ", this.state.likes), 
+                React.createElement("button", {onClick: this.handleClick, type: "button"}, "Like this")
+            )
+        )
     }
-  },
-  getInitialState: function() {
-    return {
-      greetings: 0
-    }
-  },
-  handleClick: function() {
-    this.setState({
-      greetings: this.state.greetings + 1
-    });
-  },
-  render: function () {
-    return (
-      React.createElement("div", null, 
-        React.createElement("h2", null, "Hello ", this.props.name, "!"), 
-      React.createElement("p", null, "Greetings to ", this.props.name, ": ", this.state.greetings), 
-        React.createElement("button", {onClick: this.handleClick, type: "button"}, "Say Hi!")
-      )
-    )
-  }
 });
+
 React.render(
-  React.createElement(HelloWorld, {name: "Jomo Blarrem"}),
-  document.getElementById('container')
+    React.createElement(TitleBar, null),
+    document.getElementById('container')
 );
