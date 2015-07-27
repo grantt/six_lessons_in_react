6c9c4123c7cc40af0c190d1206645fe1321435be
marsjosephine <marsjosephine@users.noreply.github.com>
2015-07-27 13:43:37 -0700

    Add gulp plumber so that gulp doesn't crash on syntax error

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,23 @@ var paths = {
     startUrl: '/lesson_1/lesson.html',
     scripts: ['lesson_*/src/*.js']
 };
+
+var handleError = function(title, message) {
+    return function() {
+        var args = Array.prototype.slice.call(arguments);
+
+        // Show notification
+        $.notify.onError({
+            title: title,
+            message: message || '<%= options.stripColor(error.message) %>',
+            templateOptions: {
+                stripColor: $.util.colors.stripColor
+            }
+        }).apply(this, args);
+
+        // Keep gulp from hanging on this task
+        this.emit('end');
+    };
 };
  
 gulp.task('server', function() {
@@ -21,6 +38,7 @@ gulp.task('server', function() {
 
 gulp.task('transform', function(){
     return gulp.src(paths.scripts)
+        .pipe($.plumber(handleError('Script Compile Error')))
         .pipe($.react())
         .pipe($.rename(function(file) {
             file.dirname = file.dirname.replace('/src', '/build');
